2eebe94df5d6deb33dcfd1fb0b87a100236155c4
Max David <me@maxdavid.com>
2022-02-21 22:11:34 -0800

    Update happy hour links

--- js/happy-hour.js
+++ js/happy-hour.js
@@ -1,8 +1,9 @@
 /* glug glug!!! */
 const today = new Date();
 const hour = today.getHours();
-const happyHour = document.querySelector('.happy-hour');
-if (14 < hour && hour < 18) {
-  console.log('Happy hour!');
-  happyHour.classList.add('active');
+const happyHour = document.querySelectorAll('.happy-hour');
+if (happyHour.length && 14 < hour && hour < 18) {
+  happyHour.forEach((block) => {
+    block.classList.add('active');
+  });
 }
